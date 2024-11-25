e851e796b35ab795d4136a6cbe41ece47e0af5d2
bangjur <azzuriptr@gmail.com>
2024-11-25 19:20:01 +0700

    update action.yml

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "HALO DUNIA ISEKAI!"
+    return "HALO DUNIA DIMANA MANUSIA BAIK BERTEBARAN!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
